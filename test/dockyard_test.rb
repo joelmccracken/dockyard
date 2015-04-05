c25562f5be7c9a3039da5f4dd38356628c4ea9b7
Joel McCracken <jmccracken@thinkthroughmath.com>
2015-04-04 23:18:03 -0400

    move names generator to test helper file, create integrationtest class

--- test/dockyard_test.rb
+++ test/dockyard_test.rb
@@ -1,29 +1,13 @@
 require 'minitest_helper'
 
-describe "docker" do
-  let(:collection) {
-    Dockyard::Docker::Collection.new
-  }
 
-  def docker
-    @docker ||= Dockyard::Docker.new do |config|
-      config.use_plugin Dockyard::Boot2Docker.new
-      config.verbose = true
-    end
-  end
-
-  after do
-    collection.images.each do |image|
-      docker.rm_image image
-    end
-  end
-
-  it "creates a new image" do
+class DockyardTest < IntegrationTest
+  it "creates a new named image" do
     Dir.mktmpdir do |dir|
       dockerfile = File.join(dir, "Dockerfile")
       File.write(dockerfile, <<-EOF.indent_heredoc)
-          FROM busybox
-          RUN echo A TEST STRING > /foobar
+        FROM busybox
+        RUN echo A TEST STRING > /foobar
       EOF
 
       image = collection.gen_image
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -8,3 +8,95 @@ class String
     gsub(/^#{scan(/^\s*/).min_by{|l|l.length}}/, "")
   end
 end
+
+
+class IntegrationTest < Minitest::Spec
+  before do
+    @namegen = FriendlyNamesGenerator.new
+    @repo_names = []
+    @container_names = []
+  end
+
+  def docker
+    @docker ||= Dockyard::Docker.new do |config|
+      config.use_plugin Dockyard::Boot2Docker.new
+      config.verbose = true
+    end
+  end
+
+  def gen_repo_name
+    name = @namegen.gen_name
+    @repo_names << name
+    name
+  end
+
+  def gen_container_name
+    name = @namegen.gen_name
+    @container_names << name
+    name
+  end
+
+  after do
+    @repo_names.each do |name|
+      docker.rm_image name
+    end
+  end
+end
+
+class FriendlyNamesGenerator
+
+  def initialize
+    adjective = %W{
+      nostalgic
+      morose
+      hungry
+      gaunt
+      satisfied
+      puzzled
+      perplexed
+      jumbo
+      nebby
+      yinz
+      ham-barbecue
+      studious
+      erudite
+      dandy
+      slippy
+      janky
+    }
+    subject = %W{
+      bigfoot
+      yetti
+      bessie
+      chupacabra
+      goblin
+      jersey-devil
+      thunder-bird
+      okapi
+      mothman
+      unicorn
+      sewer-alligator
+      spring-heeled-jack
+      tahoe-tessie
+      sasquatch
+      yowie
+      nessie
+      jackelope
+      dropbear
+      hoop-snake
+      wild-haggis
+      snipe
+    }
+    @randoms = adjective.product(subject).shuffle
+
+    @used = []
+  end
+
+  def gen_name
+    if @randoms.length == 0
+      raise "Ran out of random names!"
+    end
+
+    @randoms.shift.join "-"
+  end
+end
